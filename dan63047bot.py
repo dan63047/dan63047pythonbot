05306e1bba992442d55afd5aeb911c475032c93e
dan63047 <dan63047@gmail.com>
2020-04-12 12:47:30 +0300

    !echo fix & README update

--- dan63047bot.py
+++ dan63047bot.py
@@ -38,8 +38,21 @@ class VkBot:
         logging.info(f"Создан объект бота! id{peer_id}")
         self._USER_ID = user_id
         self._CHAT_ID = peer_id
+        self._ECHO_MODE = False
 
         self._COMMANDS = ["!image", "!my_id", "!h", "!user_id", "!group_id", "!help", "!weather", "!wiki", "!byn", "!echo"]
+
+    async def get_message(self, message):
+        if self._ECHO_MODE:
+            if message == "!echo off":
+                vk.method('messages.send', {'peer_id': self._CHAT_ID, 'message': "Режим эхо выключен", 'random_id': time.time()})
+                self._ECHO_MODE = False
+                logging.info(f"Бот id{self._CHAT_ID} вышел из режима эхо")
+            else:
+                vk.method('messages.send', {'peer_id': self._CHAT_ID, 'message': message, 'random_id': time.time()})
+                logging.info(f"Эхо-бот id{self._CHAT_ID}: {message}")
+        else:
+            await self.send_message(message)
 
     def get_weather(self, place):
         logger = logging.getLogger("dan63047bot.get_weather")
@@ -138,7 +151,7 @@ class VkBot:
         except Exception as mda:
             return "Невозможно получить данные из НБ РБ: "+str(mda)
 
-    async def new_message(self, message):
+    async def send_message(self, message):
         respond = {'attachment': None, 'text': None}
         message = message.split(' ', 1)
         if message[0] == self._COMMANDS[0]:
@@ -148,7 +161,7 @@ class VkBot:
             respond['text'] = "Ваш ид: "+str(self._USER_ID)
 
         elif message[0] == self._COMMANDS[2] or message[0] == self._COMMANDS[5]:
-            respond['text'] = "Я бот, призванный доставлять неудобства. <br>Команды:<br>!my_id - сообщит ваш id в ВК<br>!user_id *id* - сообщит информацию о этом пользователе<br>!group_id *id* - сообщит информацию о этой группе<br>!image - отправляет рандомную картинку из альбома<br>!weather *город* - отправляет текущую погоду в городе (данные из OpenWeather API)<br>!wiki *запрос* - отправляет информацию об этом из Wikipedia<br>!byn - отправляет текущий курс валют, полученный из API НБ РБ<br>!echo - бот отправляет вам всё, что вы ему пишите(работа команды не проверена)<br>!h, !help - справка<br>Дата последнего обновления: 12.04.2020<br>Проект бота на GitHub: https://github.com/dan63047/dan63047pythonbot"
+            respond['text'] = "Я бот, призванный доставлять неудобства. <br>Команды:<br>!my_id - сообщит ваш id в ВК<br>!user_id *id* - сообщит информацию о этом пользователе<br>!group_id *id* - сообщит информацию о этой группе<br>!image - отправляет рандомную картинку из альбома<br>!weather *город* - отправляет текущую погоду в городе (данные из OpenWeather API)<br>!wiki *запрос* - отправляет информацию об этом из Wikipedia<br>!byn - отправляет текущий курс валют, полученный из API НБ РБ<br>!echo - бот отправляет вам всё, что вы ему пишите<br>!h, !help - справка<br>Дата последнего обновления: 12.04.2020<br>Проект бота на GitHub: https://github.com/dan63047/dan63047pythonbot"
 
         elif message[0] == self._COMMANDS[3]:
             try:
@@ -178,14 +191,10 @@ class VkBot:
             respond['text'] = await self.exchange_rates()
 
         elif message[0] == self._COMMANDS[9]:
-            vk.method('messages.send', {'peer_id': self._CHAT_ID, 'message': "echo on", 'random_id': time.time()})
-            for event in MyVkLongPoll.listen(longpoll):
-                if event.type == VkBotEventType.MESSAGE_NEW:
-                    if event.message.text == "!echo off":
-                        respond['text'] = "echo off"
-                        break
-                    else:
-                        vk.method('messages.send', {'peer_id': self._CHAT_ID, 'message': event.message.text, 'random_id': time.time()})
+            vk.method('messages.send', {'peer_id': self._CHAT_ID, 'message': "Теперь бот работает в режиме эхо. Чтобы это выключить, введить \"!echo off\"", 'random_id': time.time()})
+            self._ECHO_MODE = True
+            logging.info(f"Бот id{self._CHAT_ID} в режиме эхо")
+            
                 
         if respond['text'] or respond['attachment']:
             message = vk.method('messages.send', {'peer_id': self._CHAT_ID, 'message': respond['text'], 'random_id': time.time(), 'attachment': respond['attachment']})
@@ -198,10 +207,10 @@ async def main():
             if event.type == VkBotEventType.MESSAGE_NEW:
                 logging.info(f'Новое сообщение в чате id{event.message.peer_id}: {event.message.text}')
                 if event.message.peer_id in bot:
-                    await bot[event.message.peer_id].new_message(event.message.text)
+                    await bot[event.message.peer_id].get_message(event.message.text)
                 else:
                     bot[event.message.peer_id] = VkBot(event.message.peer_id, event.message.from_id)
-                    await bot[event.message.peer_id].new_message(event.message.text)
+                    await bot[event.message.peer_id].get_message(event.message.text)
         except Exception as kek:
             logging.warning("Беды с ботом: "+str(kek))
             continue   
